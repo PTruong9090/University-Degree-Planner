3142575e5b825b7726f2aa55b3c127a8c66c4fd3
Phuc Truong <PTruong9090@gmail.com>
2025-12-12 01:10:44 -0800

    made to do list

--- frontend/src/components/layout/AppShell.jsx
+++ frontend/src/components/layout/AppShell.jsx
@@ -4,7 +4,7 @@ import { PlannerGrid } from "../../features/Planner/components/PlannerGrid";
 
 export function AppShell({ plan, setPlan, availableCourses, courseMap, setAvailableCourses}) {
     return (
-        <div className="flex w-full max-w-7xl h-[90vh] bg-white rounded-xl shadow-2xl overflow-hidden">
+        <div className="flex w-full border border-gray-200 max-w-7xl h-[90vh] bg-white rounded-xl shadow-2xl overflow-hidden">
             {/* Sidebar Column */}
             <Sidebar
                 availableCourses = {availableCourses}
@@ -15,8 +15,8 @@ export function AppShell({ plan, setPlan, availableCourses, courseMap, setAvaila
             {/* Main Planner Area */}
             <section className="flex-1 flex flex-col">
                 {/* Planner Head: Title and Buttons */}
-                <div className="flex justify-between items-center bg-white p-4 border-b border-gray-200 shadow-sm">
-                    <h2 className="text-xl font-semibold text-gray-800">My 4-Year Plan</h2>
+                <div className="flex justify-center items-center bg-gray-50 p-4 border-b border-gray-200 shadow-sm">
+                    <h2 className="text-2xl font-bold text-gray-800">4-Year Plan</h2>
                     <div className="flex gap-3">
                         {/* TODO: Add Export PDF JSON Buttons */}
                     </div>
@@ -31,7 +31,7 @@ export function AppShell({ plan, setPlan, availableCourses, courseMap, setAvaila
                     />
                 </div>
 
-                {/* Footer/Warnigs */}
+                {/* Footer/Warnings */}
                 <div className="bg-white p-3 border-t border-gray-200 text-xs text-gray-500 flex justify-between">
                     <span>Last Updated: Today. Please download your PDF before closing on public computers.</span>
                     {/* TODO: Custom Course Component goes here */}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,3 +9,13 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+/*
+TODO List:
+  - Whenever courses dragged into winter it appears in the wrong quarter
+  - Adding courses passes the size of the box expands the box
+  - After dragging from planner to sidebar, its not sorted to the top again
+  - Dragging into summer adds to winter
+  - Dragging into winter doesn't work
+  - Subjects doesn't work
+*/
