3467628c540755f14b87f2f9b639af54af89b1c9
Jordan Nguyen <jordanjojo1481@g.ucla.edu>
2025-12-30 23:51:49 -0800

    Add routing for Home and Login pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import PlannerPage from "./pages/PlannerPage";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage"; 
+import { NavBar } from "./features/Planner/components/NavBar";
+import { Route, Routes } from "react-router-dom";
 
 export function App() {
     return (
-        <Routes>
-            <Route path="/" element={<PlannerPage />} />
-        </Routes>
+        <div className="min-h-screen flex flex-col">
+            <NavBar />
+
+            {/* Page Content */}
+            <div className="flex-1">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/planner" element={<PlannerPage />}/>
+                </Routes>
+            </div>
+        </div>
     )
 }
