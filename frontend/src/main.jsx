88929be0e6453d629243e1c73f15be3809690915
Jordan Nguyen <jordanjojo1481@g.ucla.edu>
2025-12-30 23:53:19 -0800

    Refactor imports in main.jsx for App component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
-import App from './pages/PlannerPage.jsx'
+import { BrowserRouter } from "react-router-dom";
+import { App } from './App.jsx'
 import './index.css'
 import './styles/print.css'
 
