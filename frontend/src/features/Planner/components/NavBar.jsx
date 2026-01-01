24db2c72117ac91f9f46ab22ea04f2db050bfa60
Phuc Truong <PTruong9090@gmail.com>
2025-12-31 23:02:29 -0800

    fixed mailing authentication issue

--- frontend/src/features/Planner/components/NavBar.jsx
+++ frontend/src/features/Planner/components/NavBar.jsx
@@ -40,11 +40,11 @@ export function NavBar() {
                             </button>
                         </a>
 
-                        <a href='/login'>
+                        {/*<a href='/login'>
                             <button className="px-3 py-1.5 text-sm text-gray-800 rounded-md font-semibold hover:bg-gray-100">
                                 LOGIN/SIGNUP
                             </button>
-                        </a>
+                        </a>*/}
 
                         <button onClick={() => setIsOpen(true)} className="px-3 py-1.5 text-sm text-gray-800 rounded-md font-semibold hover:bg-gray-100">
                             CONTACT
